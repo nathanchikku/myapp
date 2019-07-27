f1f0b17b2a88d692129e62ec807641ccc0a93989
Nidhin Viswanathan <nathanchikku@gmail.com>
2019-07-28 00:18:27 +0530

    vcvcv
    dcvvcvc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+ 
+import { MatDialog, MatTable } from '@angular/material';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+ 
+export interface UsersData {
+  name: string;
+  doc: string;
+  id: number;
+}
+ 
+const ELEMENT_DATA: UsersData[] = [
+  {id: 1, name: 'Sub-Activity-Name', doc: 'yes'},
+  {id: 2, name: 'Sub-Activity-Name', doc: 'yes'},
+  {id: 3, name: 'Sub-Activity-Name', doc: 'yes'},
+  {id: 4, name: 'Sub-Activity-Name', doc: 'yes'}
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  displayedColumns: string[] = ['id', 'name', 'doc', 'action'];
+  dataSource = ELEMENT_DATA;
+ 
+  @ViewChild(MatTable,{static:true}) table: MatTable<any>;
+ 
+  constructor(public dialog: MatDialog) {}
+ 
+  openDialog(action,obj) {
+    obj.action = action;
+    const dialogRef = this.dialog.open(DialogBoxComponent, {
+      width: '750px',
+      data:obj
+    });
+ 
+    dialogRef.afterClosed().subscribe(result => {
+      if(result.event == 'Add'){
+        this.addRowData(result.data);
+      }else if(result.event == 'Delete'){
+        this.deleteRowData(result.data);
+      }
+    });
+  }
+ 
+  addRowData(row_obj){
+    var d = new Date();
+    this.dataSource.push({
+      id:d.getTime(),
+      name:row_obj.name,
+      doc:row_obj.doc
+    });
+    this.table.renderRows();``
+    
+  }
+ 
+  deleteRowData(row_obj){
+    this.dataSource = this.dataSource.filter((value,key)=>{
+      return value.id != row_obj.id;
+    });
+  }
+ 
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { 
+  MatTableModule, 
+  MatDialogModule, 
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+} from '@angular/material';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  entryComponents: [
+    DialogBoxComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
